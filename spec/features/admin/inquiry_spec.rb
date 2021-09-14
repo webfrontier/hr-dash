5c2c1efa0638919f365a93945dd3aae911a4acdc
atarubift <atarubift@gmail.com>
2021-09-14 11:28:20 +0900

    Path inquiry

--- spec/features/admin/inquiry_spec.rb
+++ spec/features/admin/inquiry_spec.rb
@@ -8,9 +8,9 @@ describe 'Admin::Inquiry', type: :feature do
   describe '#index' do
     before { visit admin_inquiries_path }
     it 'should open the index page' do
-      expect(page_title).to have_content('問い合わせ')
+      expect(page_title).to have_content('Inquiries')
       expect(page).to have_content(inquiry.referer)
-      expect(page).to have_content('編集')
+      expect(page).to have_content('Edit')
       expect(page).not_to have_css('.delete_link')
     end
   end
@@ -18,7 +18,7 @@ describe 'Admin::Inquiry', type: :feature do
   describe '#show' do
     before { visit admin_inquiry_path(inquiry) }
     it 'should open the show page' do
-      expect(page_title).to have_content("問い合わせ ##{inquiry.id}")
+      expect(page_title).to have_content("Inquiry ##{inquiry.id}")
       expect(page).to have_content(inquiry.user.name)
       expect(page).to have_content(inquiry.body)
     end
@@ -28,12 +28,12 @@ describe 'Admin::Inquiry', type: :feature do
     let(:new_admin_memo) { Faker::Lorem.paragraph }
     before do
       visit edit_admin_inquiry_path(inquiry)
-      fill_in '管理用メモ', with: new_admin_memo
-      click_on '問い合わせを更新'
+      fill_in 'inquiry[admin_memo]', with: new_admin_memo
+      click_on 'Update'
     end
 
     it 'should update the inquiry' do
-      expect(page_title).to have_content("問い合わせ ##{inquiry.id}")
+      expect(page_title).to have_content("Inquiry ##{inquiry.id}")
       expect(current_path).to eq admin_inquiry_path(inquiry)
       expect(page).to have_content(new_admin_memo)
       expect(inquiry.reload.admin_memo).to eq new_admin_memo
