9519c1e5fab6d3f46df30c3fbc03f827626074b3
atarubift <atarubift@gmail.com>
2021-09-14 11:39:51 +0900

    Path monthly_reprt

--- spec/features/admin/monthly_report_spec.rb
+++ spec/features/admin/monthly_report_spec.rb
@@ -9,7 +9,7 @@ describe 'Admin::MonthlyReport', type: :feature do
   describe '#index' do
     before { visit admin_monthly_reports_path }
     it 'should open the index page' do
-      expect(page_title).to have_content('月報')
+      expect(page_title).to have_content('Monthly Reports')
       expect(page).to have_content(user.name)
       expect(page).to have_content(report.shipped_at)
       expect(page).not_to have_css('.delete_link')
@@ -25,8 +25,8 @@ describe 'Admin::MonthlyReport', type: :feature do
     let(:new_summary) { Faker::Lorem.paragraph }
     before do
       visit edit_admin_monthly_report_path(report)
-      fill_in 'プロジェクト概要', with: new_summary
-      click_on '月報を更新'
+      fill_in 'monthly_report[project_summary]', with: new_summary
+      click_on 'Update'
     end
 
     it 'should update the monthly report' do
